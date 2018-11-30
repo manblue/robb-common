3ede0914e0aaf512521133850c48325861eef372
maolong <maolong@laiyongche.com>
2018-11-30 19:07:59 +0800

    update

---
+++ robb-service/src/main/java/com/robb/service/impl/RobbServiceImpl2.java
@@ -0,0 +1,22 @@
+package com.robb.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.robb.service.RobbService;
+
+@Service("robbServiceImpl2")
+public class RobbServiceImpl2 implements RobbService {
+
+	@Override
+	public String add(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
