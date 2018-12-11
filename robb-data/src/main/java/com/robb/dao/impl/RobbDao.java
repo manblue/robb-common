e0a218216ba01baf373fbf6e93b80bda7ec2a19b
maolong <maolong@laiyongche.com>
2018-12-11 18:34:14 +0800

    update

--- robb-data/src/main/java/com/robb/dao/impl/RobbDao.java
+++ robb-data/src/main/java/com/robb/dao/impl/RobbDao.java
@@ -1,5 +1,7 @@
 package com.robb.dao.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.robb.dao.IRobbDao;
@@ -7,7 +9,10 @@ import com.robb.dao.IRobbDao;
 @Repository("robbDao")
 public class RobbDao implements IRobbDao {
 
+	private static Logger logger  = LoggerFactory.getLogger(RobbDao.class);
 	public String add(String name) {
+		logger.info("---{} {} {}", getClass(),"add",name);
+
 		return name;
 	}
 }
